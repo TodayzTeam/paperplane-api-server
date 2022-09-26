ebfbf2c0e30c68925f0297b7065e6b249a337039
sweun2 <sweun3@gmail.com>
2022-09-26 12:32:33 +0900

    feat: Project Initialize

---
+++ src/main/java/paperplane/paperplane/PaperPlaneApplication.java
@@ -0,0 +1,13 @@
+package paperplane.paperplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperPlaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperPlaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paperplane/paperplane/PaperPlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package paperplane.paperplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperPlaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
