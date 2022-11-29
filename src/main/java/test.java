2a38bbd2a00fbe249e3840e35fa5707b8175f7e4
sweun2 <sweun3@gmail.com>
2022-11-29 20:44:54 +0900

    1234

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,10 @@
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class test {
-    @GetMapping("test1234")
+    @GetMapping("/")
     public String test(){
         return "test1234";
     }
