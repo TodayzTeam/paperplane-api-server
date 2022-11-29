ff3567c4484747c6fb95b29af113c43a4a905b79
sweun2 <sweun3@gmail.com>
2022-11-29 20:36:41 +0900

    1234

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @GetMapping("/test1234")
+    public String test(){
+        return "test1234";
+    }
+}
