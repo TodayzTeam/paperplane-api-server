e8c161380fe27c4d9ba859a57a309fcef3fa45a5
Joohee <ljh0667@gmail.com>
2023-01-19 20:33:23 +0900

    fix : Change dto

--- src/main/java/paperplane/paperplane/domain/usergroup/dto/UserGroupResponseDto.java
+++ src/main/java/paperplane/paperplane/domain/usergroup/dto/UserGroupResponseDto.java
@@ -3,6 +3,7 @@ package paperplane.paperplane.domain.usergroup.dto;
 import lombok.*;
 import paperplane.paperplane.domain.user.dto.UserResponseDto;
 import paperplane.paperplane.domain.usergroup.UserGroup;
+import paperplane.paperplane.domain.usergroup.UserRole;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,12 +17,17 @@ public class UserGroupResponseDto {
 
    private Integer groupId;
    private String groupName;
-
+   private Integer userId;
+   private String userName;
+   private UserRole userRole;
 
    public static UserGroupResponseDto of(UserGroup userGroup){
       return UserGroupResponseDto.builder()
               .groupId(userGroup.getGroup().getId())
               .groupName(userGroup.getGroup().getName())
+              .userId(userGroup.getUser().getId())
+              .userName(userGroup.getUser().getName())
+              .userRole(userGroup.getUserRole())
               .build();
    }
 
