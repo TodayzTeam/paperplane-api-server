2e04a4d2cb133cdffa290696f553ffa7d390a575
Joohee <ljh0667@gmail.com>
2023-01-21 18:20:16 +0900

    fix : Fix findGroupPost query

--- src/main/java/paperplane/paperplane/domain/post/repository/PostRepository.java
+++ src/main/java/paperplane/paperplane/domain/post/repository/PostRepository.java
@@ -35,7 +35,7 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     @Query(value = "select * from Post p where p.sender_id=:userId and p.id=:postId",nativeQuery = true)
     List<Post> test(@Param("userId") Integer userId, @Param("postId") Integer postId);
 
-    @Query(value = "select p from Post p join Group g on g.id = :groupId where p.date >= :joinDate ")
+    @Query(value = "select p from Post p join p.userPosts up on up.isReport = false where p.group.id = :groupId and p.date >= :joinDate ")
     List<Post> findGroupPost(@org.springframework.data.repository.query.Param("groupId") Integer groupId, @org.springframework.data.repository.query.Param("joinDate")LocalDateTime joinDate);
 
     @Query(value = "SELECT p FROM Post p inner join Group g on g.id=:groupId inner join p.userPosts up on up.isReport=false where((p.content like CONCAT('%',:word,'%')) or (p.title like CONCAT('%',:word,'%')) ) ")
